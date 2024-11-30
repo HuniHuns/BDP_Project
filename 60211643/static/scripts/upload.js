6c736c71b1db2829b752c9ac0ff10f2cfcd2679b
GyuriKim12 <kralicia@naver.com>
2024-11-30 17:04:51 +0900

    query 생성관련 오류 해결 및 예외처리 추가

--- 60211643/static/scripts/upload.js
+++ 60211643/static/scripts/upload.js
@@ -10,3 +10,28 @@ fileInput.addEventListener('change', () => {
         uploadName.value = '첨부파일';
     }
 });
+
+document.getElementById("uploadForm").onsubmit = function (event) {
+    event.preventDefault();  // 폼 기본 제출 방지
+
+    let formData = new FormData(this);  // 폼 데이터 생성
+
+    fetch('/upload', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.error) {
+                // 서버에서 오류가 반환된 경우 alert로 표시
+                alert("업로드의 문제가 있습니다. 다시 시도해주세요" + data.error);
+            } else {
+                // 성공 시 페이지 이동만 함
+                window.location.href = '/query';  // 업로드 후 페이지 이동
+            }
+        })
+        .catch(error => {
+            // 네트워크 오류 등 예외 처리
+            alert("네트워크 오류가 발생했습니다. 다시 시도해주세요.");
+        });
+};
