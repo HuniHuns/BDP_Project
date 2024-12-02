d508227fae95afbfb6a5dd8f366ed1c988f0d146
youngcircle-kim <dudghks021121@gmail.com>
2024-12-02 17:23:35 +0900

    fix: add the redirect and fix spark.read logic

--- 60211643/app.py
+++ 60211643/app.py
@@ -9,15 +9,9 @@ import os
 
 app = Flask(__name__)
 app.secret_key = 'BDP_project'
-# Spark 세션 생성 - Hive 지원 활성화
+# Spark 세션 생성
 spark = SparkSession.builder \
     .appName("FlaskSparkSQLApp") \
-    .enableHiveSupport() \
-    .config("spark.sql.catalogImplementation", "hive") \
-    .config("spark.hadoop.fs.defaultFS", "http://127.0.0.1:50070/webhdfs/v1/user/maria_dev") \
-    .config("spark.hadoop.fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem") \
-    .config("spark.hadoop.fs.file.impl", "org.apache.hadoop.fs.LocalFileSystem") \
-    .config("spark.hadoop.hdfs.user", HADOOP_USER_NAME) \
     .getOrCreate()
 # WebHDFS 설정
 HDFS_HOST = "http://127.0.0.1:50070"  # HDFS NameNode 주소
@@ -28,11 +22,8 @@ HDFS_UPLOAD_DIR = f"/user/{HDFS_USER}/term_project"  # 파일 업로드 경로
 QUERY_RESULT_DIR = "./query_results"
 if not os.path.exists(QUERY_RESULT_DIR):
     os.makedirs(QUERY_RESULT_DIR)
-
 # 테이블 관리
 tables = {}
-
-
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_to_hdfs():
     """파일 업로드 및 Hive 테이블 생성"""
@@ -56,16 +47,11 @@ def upload_to_hdfs():
     hdfs_path = f"{HDFS_UPLOAD_DIR}/{unique_filename}"
 
     try:
-        # HDFS에 파일 업로드
         create_url = f"{HDFS_HOST}/webhdfs/v1{hdfs_path}?op=CREATE&user.name={HDFS_USER}"
         response = requests.put(create_url, allow_redirects=False)
-
         if response.status_code != 307:
             return render_template('upload.html', tables=tables.keys(), error_message="파일 생성 초기화 실패")
-
         redirect_url = response.headers['Location']
-
-        # 첫 번째 'sandbox-hdp.hortonworks.com'만 '127.0.0.1'로 변경
         parts = redirect_url.split('sandbox-hdp.hortonworks.com', 1)
         redirect_url = '127.0.0.1'.join(parts)
         redirect_url = redirect_url.replace(
@@ -73,26 +59,20 @@ def upload_to_hdfs():
 
         with file.stream as f:
             data = f.read()
-            print(data, redirect_url)
-            response = requests.put(redirect_url, data=data, headers={
-                                    "Content-Type": "application/octet-stream"})
-            if response.status_code != 201:
-                return render_template('upload.html', tables=tables.keys(), error_message="HDFS에 파일 업로드 실패")
-        
+        response = requests.put(redirect_url, data=data, headers={
+                                "Content-Type": "application/octet-stream"})
+        if response.status_code != 201:
+            return render_template('upload.html', tables=tables.keys(), error_message="HDFS에 파일 업로드 실패")
+        file_content = data.decode('utf-8')
+        rdd = spark.sparkContext.parallelize(file_content.splitlines())
+        df = spark.read.option("header", "true").csv(rdd)
         table_name = file.filename.split('.')[0]
-        hdfs_file_path = f"hdfs://localhost:8020/user/maria_dev/term_project/{unique_filename}"
-        df = spark.read.option("header", "true") \
-            .option("inferSchema", "true") \
-            .csv(hdfs_file_path)
-        
         df = df.dropDuplicates()
         df.createOrReplaceTempView(table_name)
         if table_name not in tables:
             tables[table_name] = []
         tables[table_name].append(unique_filename)
-
-        return render_template('upload.html', tables=tables.keys(), error_message=None, success_message=f"'{file.filename}'이 성공적으로 업로드 및 테이블 생성됨")
-
+        return redirect(url_for('query_page'))
     except Exception as e:
         return render_template('upload.html', tables=tables.keys(), error_message=f"오류 발생: {str(e)}")
 
